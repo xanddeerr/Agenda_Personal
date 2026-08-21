c195d535e058bec55cb680a699f6f220c111174d
LEICA <alexander.flores08@inacapmail.cl>
2026-08-20 22:11:06 -0400

    fin de clase 20-08-2026

--- citas/admin.py
+++ citas/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cliente, Servicio
 
-# Register your models here.
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "precio", "duracion_min")
+    search_fields = ("nombre",)
+    ordering = ("nombre",)
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "telefono")
+    search_fields = ("nombre",)
+    ordering = ("nombre",)
--- citas/models.py
+++ citas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20, blank=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Servicio(models.Model):
+    nombre = models.CharField(max_length=80)
+    precio = models.IntegerField()
+    duracion_min = models.IntegerField(default=30)
+
+    def __str__(self):
+        return self.nombre
